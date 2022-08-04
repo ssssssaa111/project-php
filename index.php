7fad5653374f5008d4144df16cb3f00ca2848d96
Dalir Zhankobilov <boss.alekspey@mail.ru>
2022-08-04 18:16:55 +0300

    lesson 25

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-$date = ["year" => 2022, "month" => "August", "day" =>3];
-echo $date["year"] . "-" . $date["month"] . "-" . $date["day"];
+$arr = ['a', 'b', 'c', 'd', 'e'];
+echo $arr[count($arr)-1];
 ?>
