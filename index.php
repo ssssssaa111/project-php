8d9be42bf7ef2631fbda65f04a0e52156df66d3a
Dalir Zhankobilov <boss.alekspey@mail.ru>
2022-08-05 17:05:30 +0300

    lesson 31(№14)

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $num = 3;
 
-if ( ($num > 5 ) or ( $num > 0 and $num < 3) ) {
+if ( ($num == 9) or ( $num > 10 and $num < 20 ) or ( $num > 20 and $num < 30 ) ) {
     echo '+';
 } else {
     echo '-';
