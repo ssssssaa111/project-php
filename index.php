e55924bd8e1824bf228733a3af493e3cb185e374
Dalir Zhankobilov <boss.alekspey@mail.ru>
2022-08-07 16:25:33 +0300

    lesson 46(№2)

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
-$num = 3;
-
-if ( ($num == 9) or ( $num > 10 and $num < 20 ) or ( $num > 20 and $num < 30 ) ) {
-    echo '+';
-} else {
-    echo '-';
+for ($i=11; $i<=33; $i++) {
+    echo $i;
 }
 ?>
