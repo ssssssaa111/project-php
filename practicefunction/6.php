f45c749bc0a630ac1b1c15b048c90fb1e0db23ed
Dalir Zhankobilov <boss.alekspey@mail.ru>
2022-10-21 19:33:08 +0300

    fixed num 6

--- practicefunction/6.php
+++ practicefunction/6.php
@@ -1,12 +1,12 @@
 <?php
 //Сделайте функцию, которая будет принимать строку на русском языке, а возвращать ее транслит.
-function translit($text_for_translit)
+function translit($textForTranslit)
 {
-    $arr_rus = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я'];
-    $arr_lat = ['a', 'b', 'v', 'g', 'd', 'e', 'jo', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'cz', 'ch', 'sh', 'shh', '`', 'y', '``', 'e`', 'yu', 'ya'];
-    $arr_translit = array_combine($arr_rus, $arr_lat);
+    $arrRus = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ', 'э', 'ю', 'я'];
+    $arrLat = ['a', 'b', 'v', 'g', 'd', 'e', 'jo', 'zh', 'z', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'cz', 'ch', 'sh', 'shh', '`', 'y', '``', 'e`', 'yu', 'ya'];
+    $arrTranslit = array_combine($arrRus, $arrLat);
 
-    $translit_1 = strtr($text_for_translit, $arr_translit);
+    $translit = strtr($textForTranslit, $arrTranslit);
 }
 
-return $translit_1;
+return $translit;
