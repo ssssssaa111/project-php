258423900713d3a17b370d824d2eb4d7216e24ec
Dalir Zhankobilov <boss.alekspey@mail.ru>
2022-10-29 18:41:30 +0300

    134less

---
+++ lesson134/lesson134-1.php
@@ -0,0 +1,7 @@
+<?php
+// Сделайте форму с тремя инпутами, в которые можно ввести имя, возраст и зарплату пользователя.
+echo "<form>";
+echo "<input name='test1'>";
+echo "<input name='age'>";
+echo "<input name='salary>'";
+echo "</form>";
