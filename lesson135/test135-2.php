fbdfeb14299d3488f3e948c58bf661f1bf439bb2
Dalir Zhankobilov <boss.alekspey@mail.ru>
2022-11-02 20:55:25 +0300

    fixed+file_get

--- lesson135/test135-2.php
+++ lesson135/test135-2.php
@@ -2,8 +2,8 @@
 if (!empty($_POST['name']) and !empty($_POST['age'])) {
     $name = ($_POST['name']);
     $age = ($_POST['age']);
-    file_put_contents('file.txt', $name . ', ' . $age, FILE_APPEND);
+    file_put_contents('file.txt','   |    '. $name . ', ' . $age, FILE_APPEND);
 } else {
-    file_put_contents('file.txt', "Данные не заполнены", FILE_APPEND);
+    file_put_contents('file.txt','   |    '. 'Данные не заполнены', FILE_APPEND);
 }
 
