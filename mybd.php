895de99d29964fd6d8ebef93c38e387618860a72
overdosepriority <boss.alekspey@mail.ru>
2022-11-22 20:25:34 +0300

    mydb(fixed) from > form

--- mybd.php
+++ mybd.php
@@ -6,6 +6,6 @@ $pass = '';
 $name = 'mydb';
 $link = mysqli_connect($host, $user, $pass, $name);
 mysqli_query($link, " SET NAMES 'utf-8' ");
-$query = 'SELECT * FORM users';
+$query = 'SELECT * FROM users';
 $result = mysqli_query($link, $query) or die(mysqli_error($link));
 var_dump($result);
